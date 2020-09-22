362b1c71f206524cddbdc94ec36cd333e292566f
Nitish Kumar <54464416+kumarnu@users.noreply.github.com>
2020-09-21 23:23:23 -0500

    Add files via upload

---
+++ src/Widgets.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Widgets.css';
+
+function Widgets() {
+    return (
+      <div className="widgets">
+        <iframe
+          src="https://learnwithnitish.wordpress.com/"
+          width="340"
+          height="100%"
+          style={{ border: "none", overflow: "hidden" }}
+          scrolling="no"
+          frameborder="0"
+          allowTransparency="true"
+          allow="encrypted-media"
+        ></iframe>
+      </div>
+    );
+}
+
+export default Widgets
